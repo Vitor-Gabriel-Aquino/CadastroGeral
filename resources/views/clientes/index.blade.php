58ac9830f0d90156bf66189197852d0339e2f581
Luis Mendes <luisgustavomendesmeireles@gmail.com>
2024-02-09 22:52:17 -0300

    Novas atualizações

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,10 +1,12 @@
 <x-layout title="Clientes">
-    <a href="{{ route('clientes.create') }}" class="btn btn-dark mb-2">Adicionar cliente</a>
+    
 
     <ul class="list-group">
         @foreach ($clientes as $cliente)
             <li class="list-groups-item">{{$cliente->name}}</li>
         @endforeach
     </ul>
+    <br><br>
+    <a href="{{ route('clientes.create') }}" class="btn btn-dark mb-2">Adicionar Pessoa Fisíca</a>
 
 </x-layout>
--- resources/views/clientes/pessoaJuridicaCreate.blade.php
+++ resources/views/clientes/pessoaJuridicaCreate.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="Novo Cliente" >  
+<x-layout title="Nova Empresa" >  
 <div class="container bg-light p-4">
     <form action="{{ route('clientes.store') }}" method="post" >
         @csrf 
@@ -6,41 +6,21 @@
         <!-- Informações Pessoais -->
         <div class="row">
             <div class="col-lg-3">
-                <label for="name" class="form-label">Nome do Cliente</label>
+                <label for="name" class="form-label">Nome da Empresa</label>
                 <input type="text" class="form-control form-control-sm" id="name" name="name">
             </div>
-            <div class="col-lg-9">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control form-control-sm" id="email" name="email">
-            </div>
-        </div>
-
-        <!-- Dados de Identificação -->
-        <div class="row">
             <div class="col">
-                <label for="cpf" class="form-label">CPF</label>
-                <input type="text" class="form-control form-control-sm" id="cpf" name="cpf">
-            </div>
-            <div class="col">
-                <label for="birthdate" class="form-label">Data de Nascimento</label>
-                <input type="text" class="form-control form-control-sm" id="birthdate" name="birthdate">
+                <label for="cpf" class="form-label">Cnpj</label>
+                <input type="text" class="form-control form-control-sm" id="cnpj" name="cnpj">
             </div>
         </div>
 
         <!-- Contato -->
         <div class="row">
             <div class="col">
-                <label for="cellphone" class="form-label">Celular</label>
-                <input type="text" class="form-control form-control-sm" id="cellphone" name="cellphone">
-            </div>
-            <div class="col">
                 <label for="telephone" class="form-label">Telefone</label>
-                <input type="text" class="form-control form-control-sm" id="telephone" name="telephone">
+                <input type="text" class="form-control form-control-sm" id="company_phone" name="company_phone">
             </div>
-        </div>
-
-        <!-- Endereço -->
-        <div class="row">
             <div class="col">
                 <label for="address" class="form-label">Endereço</label>
                 <input type="text" class="form-control form-control-sm" id="address" name="address">
@@ -78,6 +58,14 @@
                 <input type="text" class="form-control" id="state" name="state">
             </div>
         </div>
+
+        <div class="row">
+            <div class="col">
+                <button class="btn ">Adicionar funcionario</button>
+            </div>
+        </div>
+
+
 
         <!-- Botão de Submissão -->
         <div class="row">
