73457a42aa8b2bd28b18f9371ff46e8e6602224f
Luis Mendes <luisgustavomendesmeireles@gmail.com>
2024-02-10 02:07:41 -0300

    Visualização em tabela

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -7,24 +7,39 @@
         </div>
     @endisset
 
-    <ul class="list-group">
-        @foreach ($clientes as $cliente)
-            <li class="list-group-item d-flex justify-content-between align-items-center">{{$cliente->name}}
-                
-                <span class="d-flex">
-                    <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-primary btn-sm">
-                        E
-                    </a>
-                    <form action="{{ route('clientes.destroy', $cliente->id)}}" method="post" class="ms-2">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger btn-sm">
-                            X
-                        </button>
-                    </form>
-                </span>
-            </li>
-        @endforeach
-    </ul>
-
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>CPF</th>
+                <th>Data de Aniversário</th>
+                <th>Celular</th>
+                <th>Endereço</th>
+                <th>CEP</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($clientes as $cliente)
+                <tr>
+                    <td>{{ $cliente->name }}</td>
+                    <td>{{ $cliente->email }}</td>
+                    <td>{{ $cliente->cpf }}</td>
+                    <td>{{ $cliente->birthdate }}</td>
+                    <td>{{ $cliente->cellphone }}</td>
+                    <td>{{ $cliente->address }}</td>
+                    <td>{{ $cliente->zipCode }}</td>
+                    <td>
+                        <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-primary btn-sm">Editar</a>
+                        <form action="{{ route('clientes.destroy', $cliente->id) }}" method="post" class="ms-2">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>
