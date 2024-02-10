424c259a14e46a02a652141b365c6dac221a22bf
Luis Mendes <luisgustavomendesmeireles@gmail.com>
2024-02-10 03:12:53 -0300

    Editar salvando todos as informações

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -2,5 +2,15 @@
     <x-clientes.form :action="route('clientes.update', $cliente->id)" 
     :name="$cliente->name"
     :email="$cliente->email"
-    :cpf="$cliente->cpf"/>
+    :cpf="$cliente->cpf"
+    :birthday="$cliente->birthday"
+    :cellphone="$cliente->cellphone"
+    :telephone="$cliente->telephone"
+    :address="$cliente->address"
+    :complement="$cliente->complement"
+    :number="$cliente->number"
+    :neighborhood="$cliente->neighborhood"
+    :zipCode="$cliente->zipCode"
+    :city="$cliente->city"
+    :state="$cliente->state"/>
 </x-layout>
